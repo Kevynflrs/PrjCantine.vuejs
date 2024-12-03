5d43ef06c6f22734d90b9cd8c3bc291f9a7c2367
Kevyn FLORESDACOSTA <kev.floresdacosta@ecoles-epsi.net>
2024-12-03 10:19:46 +0100

    Ajout d'un champ de note pour chaque plat dans le menu de cantine

--- src/App.vue
+++ src/App.vue
@@ -3,16 +3,17 @@
     <h1>Menu Cantine</h1>
   </div>
   <div>
-    <input type="text" v-model="starter" placeholder="Entrée" />
-    <input type="text" v-model="MainCourse" placeholder="Plat" />
-    <input type="text" v-model="Dessert" placeholder="Dessert" />
+    <input type="text" v-model="starter" placeholder="Entrée" /><br />
+    <input type="text" v-model="MainCourse" placeholder="Plat" /><br />
+    <input type="text" v-model="Dessert" placeholder="Dessert" /><br />
+    <input type="text" v-model="note" placeholder="Note" />
     <button @click="add">Ajouter</button>
-  </div>
+  </div> 
   <div>
     <h2>Liste des menus</h2>
     <ul>
       <li v-for="(item, index) in ListMenu" :key="index">
-        - Entrée : {{ item.starter }} - Plat : {{ item.MainCourse }} - Dessert :  {{ item.Dessert }}
+        - Entrée : {{ item.starter }} - Plat : {{ item.MainCourse }} - Dessert :  {{ item.Dessert }} - Note : {{ item.note }} / 5
       </li>
     </ul>
   </div>
@@ -24,26 +25,31 @@ import { ref } from "vue";
 const starter = ref("");
 const MainCourse = ref("");
 const Dessert = ref("");
+const note = ref("");
 
 const ListMenu = ref([
   {
     starter: "Salade",
     MainCourse: "Poulet",
     Dessert: "Tarte aux pommes",
+    note: "5",
+    
   },
 ]);
 
 const add = () => {
-  if (starter.value || MainCourse.value || Dessert.value) {
+  if (starter.value || MainCourse.value || Dessert.value || note.value) {
     ListMenu.value.push({
       starter: starter.value,
       MainCourse: MainCourse.value,
       Dessert: Dessert.value,
+      note: note.value,
     });
 
     starter.value = "";
     MainCourse.value = "";
     Dessert.value = "";
+    note.value = "";
   } else {
     alert("Veuillez remplir les champs !");
   }
